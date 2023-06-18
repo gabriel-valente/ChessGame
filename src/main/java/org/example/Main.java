5298eb7f2e8dffe09ef90e29af478f5fbb814669
Gabriel Valente <gabrielmcv1@gmail.com>
2023-06-18 22:27:00 +0100

    Added Pieces, Board and UI

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import chess.ChessMatch;
+
 public class Main {
     public static void main(String[] args) {
+        ChessMatch chessMatch = new ChessMatch();
 
+        UI.printBoard(chessMatch.getPieces());
     }
 }
