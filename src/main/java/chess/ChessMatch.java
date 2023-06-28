718ed56014125eccd76cf50b89a54267825d6f19
Gabriel Valente <gabrielmcv1@gmail.com>
2023-06-28 23:55:18 +0100

    Improve user input

--- src/main/java/chess/ChessMatch.java
+++ src/main/java/chess/ChessMatch.java
@@ -5,7 +5,6 @@ import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.*;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -106,7 +105,7 @@ public class ChessMatch {
 
     public ChessPiece replacePromotedPiece(String type) {
         if (promoted == null) throw new IllegalStateException("There is no piece to be promoted");
-        if (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) throw new InvalidParameterException("Invalid type for promotion");
+        if (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) return promoted;
 
         Position pos = promoted.getChessPosition().toPosition();
         Piece p = board.removePiece(pos);
@@ -257,7 +256,7 @@ public class ChessMatch {
         List<Piece> list = piecesOnTheBoard.stream().filter(x -> ((ChessPiece)x).getColor() == color).toList();
 
         for (Piece p : list) {
-            boolean mat[][] = p.possibleMoves();
+            boolean[][] mat = p.possibleMoves();
 
             for (int i = 0; i < board.getRows(); i++) {
                 for (int j = 0; j < board.getColumns(); j++) {
